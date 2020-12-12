0811eed3dbe38fd8abeee359445b16378a16f57c
zhangxuecheng <462436843@qq.com>
2020-12-12 14:56:30 +0800

    zero

---
+++ myslf_zero_code/src/test/java/com/fromzero/zerobeginning/ZeroBeginningApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fromzero.zerobeginning;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZeroBeginningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("1");
+    }
+
+}
