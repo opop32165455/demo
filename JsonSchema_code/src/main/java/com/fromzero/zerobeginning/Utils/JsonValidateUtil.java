1efcd4eba38fac2611068ebe4b4bbf87b145bfc5
zhangxuecheng <462436843@qq.com>
2020-12-13 19:34:34 +0800

    finished

--- JsonSchema_code/src/main/java/com/fromzero/zerobeginning/Utils/JsonValidateUtil.java
+++ JsonSchema_code/src/main/java/com/fromzero/zerobeginning/Utils/JsonValidateUtil.java
@@ -28,16 +28,14 @@ public class JsonValidateUtil {
 
     /**
      * 校验JSON
-     *
      * @param schema   json模式数据（可以理解为校验模板）
      * @param instance 需要验证Json数据
      * @return
      */
     public static ProcessingReport validatorJsonSchema(String schema, String instance) throws IOException {
         ProcessingReport processingReport = null;
-        // JsonNode jsonSchema = JsonLoader.fromResource(schema);
-        // JsonNode jsonData = JsonLoader.fromResource(instance);
-
+        // JsonNode jsonSchema = JsonLoader.fromResource("/");
+        // JsonNode jsonData = JsonLoader.fromResource("/");
         JsonNode jsonSchema = JsonLoader.fromString(schema);
         JsonNode jsonData = JsonLoader.fromString(instance);
         processingReport = factory.byDefault().getValidator().validateUnchecked(jsonSchema, jsonData);
