357474121cfa17666cbba6fcd34006cc74c304dc
zhangxuecheng <462436843@qq.com>
2020-12-17 10:35:01 +0800

    bug cant finish (tokon filter)

---
+++ shiro_zero_code/src/main/java/com/fromzero/zerobeginning/dao/SysUserDao.java
@@ -0,0 +1,14 @@
+package com.fromzero.zerobeginning.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fromzero.zerobeginning.entity.SysUser;
+
+/**
+ * (SysUser)表数据库访问层
+ *
+ * @author makejava
+ * @since 2020-12-12 14:28:35
+ */
+public interface SysUserDao extends BaseMapper<SysUser> {
+
+}
---
+++ shiro_zero_code/src/main/java/com/fromzero/zerobeginning/shiro/domain/MyShiroToken.java
@@ -0,0 +1,52 @@
+package com.fromzero.zerobeginning.shiro.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+import java.io.Serializable;
+
+/**
+ *
+ * 自定义 封装的shiro框架使用的token
+ *
+ * * 父类属性
+ * * @param private String username;
+ * * @param private char[] password;
+ * * @param  private boolean rememberMe;
+ * * @param  private String host;
+ * @author lin
+ * @since 2017/4/7.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyShiroToken extends UsernamePasswordToken implements Serializable {
+
+
+    private String tokenCustomAttribute1;
+
+    private String tokenCustomAttribute2;
+
+    private String tokenCustomAttribute3;
+
+    private String tokenCustomAttribute4;
+
+    public MyShiroToken(String email, String password) {
+        super(email,password);
+    }
+
+    public MyShiroToken(String username, String password, boolean aBoolean, String host, String tokonFilter1, String tokonFilter2) {
+        super(username,password,aBoolean,host);
+        this.tokenCustomAttribute1=tokonFilter1;
+        this.tokenCustomAttribute2=tokonFilter2;
+    }
+
+    /**
+     * 因为父类的password是char类型 这里强行写成string 避免麻烦
+     */
+    public String getStringPassword() {
+        return String.valueOf(this.getPassword());
+    }
+}
