7bc49462bc478fe0a64b8d7be4fa6f723df63048
zhangxuecheng <zxc1996720>
2021-08-14 14:49:29 +0800

    fix all

---
+++ Listener/src/main/java/com/zero/listener/ListenerApplication.java
@@ -0,0 +1,16 @@
+package com.zero.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class ListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerApplication.class, args);
+    }
+
+}
---
+++ Listener/src/test/java/com/zero/listener/ListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
